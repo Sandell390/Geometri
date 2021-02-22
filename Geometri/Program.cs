426c65ae413426fe925d7fafb97dcf27f7a3a9dd
Jesper Sandell <sandell327@gmail.com>
2021-02-22 09:41:41 +0100

    Add project files.

---
+++ Geometri/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            square square = new square(6);
+
+            Console.WriteLine($"Omkreds: {square.omkreds()}");
+            Console.WriteLine($"Areal: {square.areal()}");
+
+            square.Side = 4;
+
+            Console.WriteLine($"Omkreds: {square.omkreds()}");
+            Console.WriteLine($"Areal: {square.areal()}");
+
+            square.Side = 12;
+
+            Console.WriteLine($"Omkreds: {square.omkreds()}");
+            Console.WriteLine($"Areal: {square.areal()}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Geometri/square.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometri
+{
+    class square
+    {
+
+        private int side;
+
+        public int Side 
+        {
+            set 
+            {
+                this.side = value;
+            }
+            get 
+            {
+                return this.side;
+            }
+        }
+
+        public square(int _side) 
+        {
+            side = _side;
+        }
+
+        public int omkreds() 
+        {
+            int omkreds = side * 4;
+
+
+            return omkreds;
+        }
+
+        public int areal() 
+        {
+            int areal = side * side;
+
+            return areal;
+        }
+
+    }
+}
